2312c578e4a401ffe25706136179bcbf80a8cb06
Amrullah Zunzunia <amrullah.zunzunia@gmail.com>
2022-03-06 12:39:43 +0530

    Product Crud API's

--- products_service/products/tests.py
+++ products_service/products/tests.py
@@ -1,13 +1,62 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 from .models import Product
 
 
-class ProductTests(TestCase):
+class ProductTests(APITestCase):
     def test_product_creation_and_querying(self):
         product = Product(sku='something', name='Superb', description='Mind Blowing, no words')
         product.save()
         self.assertEqual(Product.objects.count(), 1, msg="One product should have been created")
-        print("\n\n")
-        print(Product.objects.all().explain(verbose=True, analyze=True))
-        print("\n\n")
+        # print("\n\n")
+        # print(Product.objects.all().explain(verbose=True, analyze=True))
+        # print("\n\n")
+
+    def test_product_crud_apis(self):
+        url = "/products/"
+        data = {
+            'sku': 'something',
+            'name': 'Something unbelievable',
+            'description': 'Mind Blowing, speechless'
+        }
+        response = self.client.post(url, data, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.get(url, format='json')
+        print(response.data)
+
+        response = self.client.get(url + "something/", format='json')
+        print(response.data)
+
+        data = {
+            'description': 'Something you can\'t believe'
+        }
+        response = self.client.patch(url + "something/", data, format='json')
+        print(response.data)
+        print()
+        response = self.client.get(url + "something/", format='json')
+        print(response.data)
+
+        data = {
+            'sku': 'something-else',
+            'name': 'mind boggling',
+            'description': 'itna mast ke poocho mat'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.data['count'], 2)
+
+        self.client.delete(url + "something/", format='json')
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.data['count'], 1)
+
+        self.client.delete(url + "delete_all/", format='json')
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.data['count'], 0)
+
+
--- products_service/products/views.py
+++ products_service/products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Product
+from .pagination_classes import HundredResultsSetPagination
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    # http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = Product.objects.all()  # TODO: unordered queryset
+    serializer_class = ProductSerializer
+    pagination_class = HundredResultsSetPagination
+
+    @action(methods=['delete'], detail=False)
+    def delete_all(self, request):
+        Product.objects.all().delete()
+        return Response(status=HTTP_204_NO_CONTENT)
